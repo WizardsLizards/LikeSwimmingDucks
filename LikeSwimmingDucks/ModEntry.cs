62d8ad92f7d44a938071182cb9b64642159f7cb3
WizardsLizards <focdom@gmail.com>
2022-02-24 20:10:37 +0100

    Init

---
+++ LikeSwimmingDucks/ModEntry.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using System;
+
+namespace LikeSwimmingDucks
+{
+    public class ModEntry : StardewModdingAPI.Mod
+    {
+        public override void Entry(IModHelper IHelper)
+        {
+            #region Setup
+
+            #endregion Setup
+
+            #region Harmony Patches
+
+            #endregion Harmony Patches
+
+            #region Events
+
+            #endregion Events
+        }
+    }
+}
